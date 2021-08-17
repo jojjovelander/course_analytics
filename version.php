7b27a0eb4754f28f320f50b94af28ecd760804df
Johanna Velander <johanna@adhibit.se>
2021-08-17 23:20:19 +0200

    Add mustache to version.php

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
 $plugin->component = 'block_course_analytics';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
-$plugin->version = 2011062821;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = {{ version }};  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2019111802; // YYYYMMDDHH (This is the release version for Moodle 2.0)
