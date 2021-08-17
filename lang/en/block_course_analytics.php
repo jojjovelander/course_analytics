bd253c22779c7c78dac76d3fed4e83a7d1d7bd04
Johanna Velander <johanna@adhibit.se>
2021-08-17 23:20:53 +0200

    Add attributes to plugin

--- lang/en/block_course_analytics.php
+++ lang/en/block_course_analytics.php
@@ -1,7 +1,5 @@
 <?php
-$string['pluginname'] = 'Course Analytics';
-
-
-
-
-
+$string['pluginname'] = 'Course Analytics Block';
+$string['course_analytics'] = 'Course Analytics';
+$string['course_analytics:addinstance'] = 'Add a new Course Analytics block';
+$string['course_analytics:myaddinstance'] = 'Add a new Course Analytics block to the My Moodle page';
